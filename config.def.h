8f48fca0ce807f4e865f027bb2ef8b8e9c3ae6bf
Jorri Fransen <jorri@jorrifransen.nl>
2021-02-09 16:24:02 +0100

    Decreasing snap to 8.

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 12;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 8;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
