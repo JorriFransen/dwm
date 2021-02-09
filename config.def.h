42c41d4cd35c69de594356c142b838d135a6a582
Jorri Fransen <jorri@jorrifransen.nl>
2021-02-09 16:18:20 +0100

    My custom config.

--- config.def.h
+++ config.def.h
@@ -17,13 +17,14 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_orange[]      = "#ff8c1a";
+static const char col_orange[]      = "#ff9933";
+static const char col_dark_cyan[]   = "#2db095";
 static const unsigned int baralpha = 185;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_orange  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_dark_cyan  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
